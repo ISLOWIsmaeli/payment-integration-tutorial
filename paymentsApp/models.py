b449ca8696921b942a180c55b5e81bc0f725dd3e
ISLOWIsmaeli <Lenojore67@gmail.com>
2025-11-16 13:37:05 +0300

    finished working on the dynamic donation feature

--- paymentsApp/models.py
+++ paymentsApp/models.py
@@ -8,5 +8,6 @@ class DonationHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     donation_id = models.CharField(max_length=225, default=uuid.uuid4())
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     donation_status = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
--- projectsApp/models.py
+++ projectsApp/models.py
@@ -5,7 +5,6 @@ class Project(models.Model):
 
     name = models.CharField(max_length=225)
     description = models.TextField()
-    donation = models.IntegerField()
     image = models.URLField(blank=True, null=True)
 
     def __str__(self):
